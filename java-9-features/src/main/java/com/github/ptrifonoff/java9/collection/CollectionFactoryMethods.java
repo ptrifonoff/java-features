1269e634d85ee5cede857a97a91650bb4c0d9424
Philipp Trifonoff <philipp.trifonoff@itsv.at>
2020-04-10 12:07:50 +0200

    demonstrating collection factory methods of JAVA 9

---
+++ java-9-features/src/main/java/com/github/ptrifonoff/java9/collection/CollectionFactoryMethods.java
@@ -0,0 +1,20 @@
+package com.github.ptrifonoff.java9.collection;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class CollectionFactoryMethods {
+
+    public static void main(String[] args) {
+        List<String> stringListCreatedWithOfFactory = List.of("Element 1", "Element 2", "Element 3");
+        System.out.println("Liste = " + stringListCreatedWithOfFactory);
+
+        Set<Integer> integerSetCreatedWithOfFactory = Set.of(1,2,3);
+        System.out.println("Set = " + integerSetCreatedWithOfFactory);
+
+        Map<String, Integer> mapCreatedWithOfFactory = Map.of("Key 1", 1, "Key 2", 2);
+        System.out.println("Map = " + mapCreatedWithOfFactory);
+    }
+    
+}
