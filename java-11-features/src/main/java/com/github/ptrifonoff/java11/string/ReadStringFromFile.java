6ab3d61092d1c5cdc9808cd76cef46d9ba72dbbc
Philipp Trifonoff <philipp.trifonoff@itsv.at>
2020-07-01 15:58:02 +0200

    Java 11 TODOs for marking new features added

--- java-11-features/src/main/java/com/github/ptrifonoff/java11/string/ReadStringFromFile.java
+++ java-11-features/src/main/java/com/github/ptrifonoff/java11/string/ReadStringFromFile.java
@@ -8,6 +8,7 @@ import java.nio.file.Path;
 public class ReadStringFromFile {
 
     public static void main(String[] args) throws IOException {
+        // TODO Read String from a file
         String fileContent = Files.readString(Path.of("./java-11-features/src/main/resources/testfile.txt"), StandardCharsets.UTF_8);
         System.out.println("*** File Content ***");
         System.out.println(fileContent);
